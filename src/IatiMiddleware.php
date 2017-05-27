8296e91ba613e68fd85930d359f0305a788827e6
Mono Cidar <monocidar@users.noreply.github.com>
2017-05-27 15:22:55 +0300

    Create IatiMiddleware.php

---
+++ src/IatiMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+namespace SiyahKare\Iati;
+
+use Closure;
+
+class IatiMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}

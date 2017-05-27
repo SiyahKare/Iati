c78c5cfb46497c7ab669afb7a1375284193b642e
Mono Cidar <monocidar@users.noreply.github.com>
2017-05-27 15:09:26 +0300

    Create IatiClass.php

---
+++ src/IatiClass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SiyahKare\Iati;
+
+class IatiClass
+{
+    /**
+     * Create a new Iati Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
+}
